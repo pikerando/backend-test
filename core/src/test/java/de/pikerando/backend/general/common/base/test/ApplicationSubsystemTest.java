46a2aba6a9dd161bc5d7180b6044c48b690bf7d5
Patrick Vogel <patrick.vogel@capgemini.com>
2022-09-08 14:24:49 +0200

    Initial devon4j application

---
+++ core/src/test/java/de/pikerando/backend/general/common/base/test/ApplicationSubsystemTest.java
@@ -0,0 +1,16 @@
+package de.pikerando.backend.general.common.base.test;
+
+import com.devonfw.module.test.common.base.SubsystemDbTest;
+
+import de.pikerando.backend.SpringBootApp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+/**
+ * Abstract base class for {@link SubsystemTest}s of this application.
+ */
+@SpringBootTest(classes = { SpringBootApp.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class ApplicationSubsystemTest extends SubsystemDbTest {
+
+}
